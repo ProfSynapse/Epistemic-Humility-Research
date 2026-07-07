57f9fb873538750fb19302e2bbcd5a6d9dcf6a20
Professor Synapse <131487882+ProfSynapse@users.noreply.github.com>
2026-07-07 10:42:46 -0400

    Use Modal Python for AQ wrapper image

--- experiments/aq-sycophancy-activation-actuator/cloud/modal_aq_sycophancy_activation_actuator.py
+++ experiments/aq-sycophancy-activation-actuator/cloud/modal_aq_sycophancy_activation_actuator.py
@@ -107,9 +107,8 @@ def build_spec(repo_commit: str, cost_cap_usd: float | None) -> dict:
 
 if modal is not None:
     image = (
-        modal.Image.from_registry(VLLM_IMAGE, add_python=None)
+        modal.Image.from_registry(VLLM_IMAGE, add_python="3.12")
         .entrypoint([])
-        .run_commands("ln -sf /usr/local/bin/python3 /usr/local/bin/python")
         .run_commands("python3 -m pip install " + " ".join(shlex.quote(dep) for dep in PIP_DEPS))
         .apt_install("git")
         .env({"HF_HUB_DISABLE_XET": "1", "HF_HUB_ENABLE_HF_TRANSFER": "0"})
